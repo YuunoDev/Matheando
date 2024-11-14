9ad0714b0b2bee357788563af3323a4dd85351da
YuunoDev <adrianalonso.a4@proton.me>
2024-11-14 14:46:11 -0600

    Cambio de errores y cambio de progressbar

---
+++ app/src/main/java/com/aaa/matheando/progress.java
@@ -0,0 +1,46 @@
+package com.aaa.matheando;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class progress extends LinearLayout {
+    ProgressBar progressBar;
+    TextView TextProgreso;
+    Button btnGuardarProgreso;
+    private int progreso = 0;
+
+    public progress(Context context) {
+        super(context);
+        init();
+    }
+
+    public progress(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init(){
+        LayoutInflater.from(getContext()).inflate(R.layout.progress, this, true);
+        progressBar=findViewById(R.id.progressBar);
+        TextProgreso=findViewById(R.id.TextProgreso);
+        btnGuardarProgreso=findViewById(R.id.btnGuardarProgreso);
+    }
+
+    public void setProgreso(int progreso) {
+        this.progreso = progreso;
+        progressBar.setProgress(progreso);
+        TextProgreso.setText(progreso + "%");
+    }
+
+    public int getProgreso() {
+        return progreso;
+    }
+}
